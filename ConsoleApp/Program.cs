020690f5c26f09363418629788027c86c393bba7
guto401 <87216391+guto401@users.noreply.github.com>
2026-07-15 10:48:47 -0300

    Criando um hello world para testar consoleapp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+AnsiConsole.Write(
+    new FigletText(@"infoX by @guto401")
+    .Centered()
+    .Color(Color.Blue)
+);
+
+AnsiConsole.MarkupLine("\n[bold yellow]Hello world :)[/]");
