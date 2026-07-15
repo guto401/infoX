b2c0c06ee3db656d715995e1bca7aa33a6f5a442
guto401 <87216391+guto401@users.noreply.github.com>
2026-07-15 12:34:16 -0300

    Corringindo uma parte das cagadas

--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac540599b3e88fa28e12accbfa0502582267e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf548fc5d5a907ec7c4ae190cef72e973e452d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
