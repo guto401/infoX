4f3b11c0496435a82dc134431860b555ccd09666
guto401 <87216391+guto401@users.noreply.github.com>
2026-07-15 11:49:37 -0300

    Update Usuario.cs
    Pequeno ajuste

--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -3,7 +3,13 @@
     public class Usuario
     {
         public int Id { get; set; }
-        public int Nome { get; set; }
+        public string Nome { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+
+        public Usuario(string nome, string passwordHash)
+        {
+            nome = Nome;
+            passwordHash = PasswordHash;
+        }
     }
 }
